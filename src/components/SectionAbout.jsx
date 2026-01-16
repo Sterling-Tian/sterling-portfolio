24f8cea7572a59768c8b1fc4c60336a59c65a565
Sterling Tian <Sterling.Tian@gmail.com>
2026-01-16 13:04:04 -0600

    ResumeButton

--- src/components/SectionAbout.jsx
+++ src/components/SectionAbout.jsx
@@ -67,7 +67,8 @@ export const SectionAbout = () =>{
                     </p>
                     <div className="flex flex-col sm:flex-row gap-4 pt-10 justify-center">
                         <a
-                            href="#contact"
+                            href="/SterlingResume2026.pdf"
+                            download
                             className={cn("px-6 py-2 rounded-full border border-primary text-primary",
                             "hover:bg-primary/10 transition-colors duration-300")}>
                             Resume
